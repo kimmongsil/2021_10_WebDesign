4c70875b6ca7d36d6e9e5ba0bff853fe364e202c
kimmongsil <g.rlatp1999@gmail.com>
2021-11-07 10:54:13 +0900

    팝업

---
+++ 07-팝업/script/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $('.layerPopup').click(function(){
+        $(".layer").show();
+        $(".layer-bg").show();
+        // $('.layer').css('display', 'block');
+        // $('.layer-bg').css('display', 'block');
+    });
+
+    $('.close-btn').click(function(){
+        $('.layer').hide();
+        $('.layer-bg').hide();
+    });
+
+});
